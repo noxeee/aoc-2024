8f89f1e28ef7b2fe9e57095cca5b5da5b6c39d52
Lujza Lanyi <l.lanyi@pineit.at>
2024-12-02 08:48:32 +0100

    completed day1 puzzle

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,78 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        // Or read a large test input from the `src/Day01_test.txt` file:
+        val leftList : MutableList<Int> = mutableListOf();
+        val rightList : MutableList<Int> = mutableListOf();
+        //testInput.get(0).println();
+        input.forEach {
+            i -> val split = i.split("\\s+".toRegex());
+            leftList.add(split.get(0).toInt());
+            rightList.add(split.get(1).toInt());
+        }
+        print("Left list");
+        leftList.println();
+        print("right list");
+        rightList.println();
+
+        leftList.sort();
+        rightList.sort();
+
+        println("sorted list");
+        leftList.println();
+
+        var sum : Int = 0;
+        while (!leftList.isEmpty() and !rightList.isEmpty()) {
+            var diff = leftList.first().minus(rightList.first());
+            println("diff with negatives");
+            println(diff);
+            if (diff < 0) {
+                diff *= -1;
+            }
+            println("diff after * -1");
+            println(diff);
+            sum = sum.plus(diff);
+            rightList.removeFirst();
+            leftList.removeFirst();
+        }
+        return sum;
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+            // Or read a large test input from the `src/Day01_test.txt` file:
+            val leftList : MutableList<Int> = mutableListOf();
+            val rightList : MutableList<Int> = mutableListOf();
+            //testInput.get(0).println();
+            input.forEach {
+                    i -> val split = i.split("\\s+".toRegex());
+                leftList.add(split.get(0).toInt());
+                rightList.add(split.get(1).toInt());
+            }
+            print("Left list");
+            leftList.println();
+            print("right list");
+            rightList.println();
+
+            leftList.sort();
+            rightList.sort();
+
+            println("sorted list");
+            leftList.println();
+
+            var sum : Int = 0;
+            while (!leftList.isEmpty()) {
+                var occ = rightList.count{it == leftList.first()};
+                //print(leftList.removeFirst() * occ);
+                sum = sum.plus(leftList.removeFirst() * occ);
+            }
+            return sum;
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    //check(part1(listOf("test_input")) == 1)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val testInput = readInput("Day01")
+    //val testInput = readInput("exampleInput");
+    println("final distance");
+    //part1(testInput).println();
+    part2(testInput).println();
 }
