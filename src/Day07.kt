e6d4286c62ab44db26ce3c888c7c7a5db546930c
noxeee <lujza.la@gmail.com>
2024-12-07 20:47:17 +0100

    day7 part2 solved

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,9 +1,11 @@
 import java.util.*
+import kotlin.concurrent.timer
 import kotlin.io.path.Path
 import kotlin.io.path.readText
 import kotlin.math.ceil
 import kotlin.math.exp
 import kotlin.math.floor
+import kotlin.time.measureTime
 
 fun main() {
 
@@ -27,8 +29,7 @@ fun main() {
         return result
     }
 
-    fun getOpCombinations(numSz: Int): List<String> {
-        val operators = listOf('*', '+')
+    fun getOpCombinations(operators: List<Char>, numSz: Int): List<String> {
         var combinations = mutableListOf<String>()
 //        for (len in 0..numSz) {
 //            val combo = genCombinations(operators, len)
@@ -40,7 +41,7 @@ fun main() {
     }
 
     fun solver(nums: List<Int>, result : Long) : Boolean {
-        val combinations: List<String> = getOpCombinations(nums.size-1)
+        val combinations: List<String> = getOpCombinations(listOf('*','+'), nums.size-1)
         println("combinations")
         combinations.println()
         var computedResult : Long = -1
@@ -58,6 +59,40 @@ fun main() {
                             computedResult = nums[idx].toLong()
                         }
                         computedResult += nums[idx+1]
+                }
+            }
+            println("computedResult ${computedResult}")
+            if (computedResult == result) {
+                return true
+            }
+        }
+        return false
+    }
+
+    fun solverConcat(nums: List<Int>, result : Long) : Boolean {
+        val combinations: List<String> = getOpCombinations(listOf('*','+','|'), nums.size-1)
+        println("combinations")
+        combinations.println()
+        var computedResult : Long = -1
+        combinations.forEach {
+                combo ->
+            computedResult = -1
+            for (idx in 0..nums.size-2) {
+                if (combo.length > 1 && combo[idx] == '*' || combo.equals("*")) {
+                    if (computedResult.toInt() == -1) {
+                        computedResult = nums[idx].toLong()
+                    }
+                    computedResult *= nums[idx+1]
+                } else if (combo.length > 1 && combo[idx] == '+' || combo.equals("+")) {
+                    if (computedResult.toInt() == -1) {
+                        computedResult = nums[idx].toLong()
+                    }
+                    computedResult += nums[idx+1]
+                } else if (combo[idx] == '|') {
+                    if (computedResult.toInt() == -1) {
+                        computedResult = nums[idx].toLong()
+                    }
+                    computedResult = (computedResult.toString() + nums[idx+1].toString()).toLong()
                 }
             }
             println("computedResult ${computedResult}")
@@ -88,12 +123,32 @@ fun main() {
         return sum;
     }
 
+    fun part2(input: List<String>): Long {
+        input.forEach { i -> i.println() }
+        var sum: Long = 0;
+        input.forEach{
+                i ->
+            val split = i.split(":")
+            val result = split[0].toLong()
+            val nums = split[1].trim().split(" ")
+            val numInts = nums.map { j -> j.toInt() }
+            println(nums)
+            val solved = solverConcat(numInts, result)
+            if (solved) {
+                sum += result
+                sum.println()
+            }
+
+        }
+        return sum;
+    }
 
     val testInput = readInput("Day07")
     //val testInput = readInput("exampleInput");
 
     println("result part 1:");
-    part1(testInput).println();
+    //part1(testInput).println();
     println("result part2")
-    //part2(testInput.toMutableList()).println();
+    measureTime { part2(testInput.toMutableList()).println(); }.println()
+
 }
